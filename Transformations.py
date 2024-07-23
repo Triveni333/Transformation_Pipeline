d9b54b1d15ad025bd38967b8c734fe9e24a56649
Triveni333 <trivenichitturi333@gmail.com>
2024-07-23 14:50:45 -0400

    Third transformation

--- Transformations.py
+++ Transformations.py
@@ -2,6 +2,8 @@ import pandas as pd
 data = pd.read_csv('machine-readable-business-employment-data-mar-2024-quarter(in).csv')
 
 one_data = data.drop(columns=['Suppressed','Series_title_4', 'Series_title_5'])
+print(one_data)
+
 one_data['Period'] = one_data['Period'].astype(str)
 one_data['year'] = one_data['Period'].str.split('.').str[0]
 two_data = one_data.groupby(['year','Series_reference']).Data_value.sum().reset_index()
@@ -17,10 +19,10 @@ for i, row in two_data.iterrows():
         mapping_dict[row['Series_reference']]['year'] = row['year']
         mapping_dict[row['Series_reference']]['Data_value'] = row['Data_value']
 
-print(mapping_dict)
 
 transform = pd.DataFrame.from_dict(mapping_dict, orient='index', columns=['year', 'Data_value'])
 print(transform)
     
-#third_data = one_data.Data_value.isnull().groupby('Series_reference')
-#print(third_data)
+third_data = data[data.Data_value.notnull()]
+third_data.to_csv('No missing data values.csv')
+print(third_data)
